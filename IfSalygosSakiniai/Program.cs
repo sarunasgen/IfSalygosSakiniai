6d24cc397d3b795ff02f8499300b6ac7195e3ed9
Sarunas <sarugen12@gmail.com>
2024-10-22 08:41:08 +0200

    Add project files.

---
+++ IfSalygosSakiniai/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace IfSalygosSakiniai
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite A reiksme: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Iveskite B reiksme: ");
+            int b = int.Parse(Console.ReadLine());
+
+            if(a > b)
+            {
+                Console.WriteLine("A daugiau uz B");
+            }
+            else if(a < b)
+            {
+                Console.WriteLine("A maziau uz B");
+            }
+            else
+            {
+                Console.WriteLine("A yra lygu B");
+            }
+            
+        }
+    }
+}
